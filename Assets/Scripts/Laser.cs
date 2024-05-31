c9c92a0cf02d4a052aeb2c6dbd234f60081098fc
qresgg <ernestocarmonegg@gmail.com>
2024-05-31 13:19:13 +0300

    style: Clean up code

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -6,19 +6,11 @@ public class Laser : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 8f;
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
 
-    // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.up * _speed * Time.deltaTime);
-
-        if (transform.position.y > 8f)
-        {
-            Destroy(this.gameObject);
-        }       
+        
+        if (transform.position.y > 8f ? true : false) Destroy(this.gameObject);
     }
 }
