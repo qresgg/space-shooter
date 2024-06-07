1af1c44d9f350e6113b96a2483550eeb85ac1da4
qresgg <ernestocarmonegg@gmail.com>
2024-06-07 16:38:15 +0300

    feat: game over text and restart level

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private bool _isGameOver;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R) && _isGameOver == true)
+        {
+            SceneManager.LoadScene(0);
+        }   
+    }
+
+    public void GameOver()
+    {
+        _isGameOver = true;
+    }
+}
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -9,10 +9,15 @@ public class UIManager : MonoBehaviour
     [SerializeField] private TMP_Text _scoreText;
     [SerializeField] private Sprite[] _livesSprites;
     [SerializeField] private Image _liveImg;
-    private int _scoreFrom = 0;
+    [SerializeField] private TMP_Text _gameOverText;
+    [SerializeField] private TMP_Text _restartKeyText;
+    private GameManager _gameManager;
     // Start is called before the first frame update
     void Start()
     { 
+        _scoreText.text = "Score: " + 0;
+        _gameOverText.gameObject.SetActive(false);
+        _gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
     // Update is called once per frame
     
@@ -23,5 +28,22 @@ public class UIManager : MonoBehaviour
     public void updateLives (int currentLive)
     {
         _liveImg.sprite = _livesSprites[currentLive];
+        if(currentLive == 0) gameOver();
+    }
+    void gameOver(){
+        _gameOverText.gameObject.SetActive(true);
+        _restartKeyText.gameObject.SetActive(true);
+        StartCoroutine(GameOverFlickerRoutine());
+        _gameManager.GameOver();
+    }
+    IEnumerator GameOverFlickerRoutine()
+    {
+        while(true)
+        {
+            _gameOverText.text = "GAME OVER";
+            yield return new WaitForSeconds(0.5f);
+            _gameOverText.text = "";
+            yield return new WaitForSeconds(0.5f);
+        }
     }
 }
